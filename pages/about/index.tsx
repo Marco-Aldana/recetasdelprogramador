172e4633e13922e9c64045855ed72bc29216511a
Marco Aldana <marco.aldana.d@gmail.com>
2022-04-15 06:14:42 -0500

    social buttons added

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,6 +1,8 @@
 import { Grid } from "@mui/material";
 import { NextPage } from "next";
 import Layout from "../../components/Layout";
+import MainMenuList from "../../components/utils/MainMenuList";
+import socialMenu from "../../dummy/socialMenu";
 import styles from '../../styles/About.module.css'
 
 const About: NextPage = () => {
@@ -10,6 +12,7 @@ const About: NextPage = () => {
                 <p className={styles.subtitle}>pagina en construccion...</p>
                 <img src="https://esp.habitants.org/var/ezwebin_site/storage/images/la_via_urbana/alojar_mil_millones_de_personas2/work_in_progress/2390822-1-ita-IT/work_in_progress.jpg" alt="Trabajo en progreso" />
             </Grid>
+            <MainMenuList open={true} items={socialMenu}></MainMenuList>
             <Grid container >
                 <iframe src="https://marco-aldana.github.io/" name="targetframe" className={styles.main} />
             </Grid>
